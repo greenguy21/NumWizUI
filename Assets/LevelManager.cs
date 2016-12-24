018f4bb8aec8b1e4b3c34f944ab29a1d01b3fb55
Pranay Shoroff <mountainowlbiker@gmail.com>
2016-12-23 22:12:22 -0800

    Near Final Push
    All functionality finished. Most scaling and positioning done. Basic
    debugging finished. Need to finish scaling and check all screen
    functionality.

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel (string name) {
+		Application.LoadLevel (name);
+	}
+
+	public void QuitRequest() {
+		Debug.Log ("Quit Requested");
+		Application.Quit ();
+	}
+}
---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+
+	//Vars
+	int max, min, guess;
+	int maxGuessesAllowed = 10;
+	public Text guessText;
+
+	// Use this for initialization
+	void Start() {
+		max = 1001;
+		min = 1;
+		guess = 500;
+		guessText.text = guess.ToString();
+	}
+		
+	public void GuessHigher() {
+		min = guess;
+		NextGuess ();
+	}
+
+	public void GuessLower() {
+		max = guess;
+		NextGuess();
+	}
+		
+	void NextGuess(){
+		guess = (min + max) / 2;
+		guessText.text = guess.ToString();
+		maxGuessesAllowed--;
+		print(maxGuessesAllowed.ToString());
+		if (maxGuessesAllowed <= 0) {
+			Application.LoadLevel("Win");
+		}
+	}
+}
